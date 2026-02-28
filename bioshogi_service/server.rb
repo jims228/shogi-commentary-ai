700e7ee9ed58a88ef3ba0c1fca7976756e40ffe8
jims228 <shingoakito.3@gmail.com>
2026-02-28 17:42:12 +0900

    feat: add bioshogi Sinatra microservice

---
+++ bioshogi_service/server.rb
@@ -0,0 +1,38 @@
+require "sinatra"
+require "json"
+require "bioshogi"
+
+set :port, 7070
+set :bind, "0.0.0.0"
+
+post "/analyze" do
+  content_type :json
+  body = JSON.parse(request.body.read)
+  kifu = body["kifu"] || ""
+  
+  begin
+    parser = Bioshogi::Parser.parse(kifu, {
+      typical_error_case: :embed,
+      analysis_feature: true
+    })
+    
+    result = parser.container.players.map.with_index do |player, i|
+      tags = player.tag_bundle.to_h
+      {
+        player: i,
+        attack:    tags[:attack]    || [],
+        defense:   tags[:defense]   || [],
+        technique: tags[:technique] || [],
+        note:      tags[:note]      || []
+      }
+    end
+    
+    { ok: true, players: result }.to_json
+  rescue => e
+    { ok: false, error: e.message }.to_json
+  end
+end
+
+get "/health" do
+  { ok: true }.to_json
+end
